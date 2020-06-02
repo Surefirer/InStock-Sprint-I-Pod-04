a13fafc528cb7e47aa12476fde1fd4415a0cc471
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-02 09:39:53 -0700

    1.change fist letters on inventory and location to uppercase in app.js file. 2. add 'gitignore' file in server to ignore node_modules push to git

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import inventory from "./components/pages/Inventory";
-import location from "./components/pages/Location";
+import Inventory from "I./components/pages/Inventory";
+import Location from "./components/pages/Location";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/inventory" component={inventory} />
-        <Route path="/location" component={location} />
+        <Route path="/inventory" component={Inventory} />
+        <Route path="/location" component={Location} />
       </Switch>
     </Router>
   );
