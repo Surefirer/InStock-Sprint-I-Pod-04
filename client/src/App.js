e96a00bbcf8edc6f64181c935514900a620d09b4
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-02 09:51:46 -0700

    1.fix extra "I' in the app.js line 4 2.delete node_modules file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import Inventory from "I./components/pages/Inventory";
+import Inventory from "./components/pages/Inventory";
 import Location from "./components/pages/Location";
 
 function App() {
