ad1f52740538bb4a7e9bdcd1ee3f30b7a09ad873
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-06-02 17:10:21 -0700

    initial warehouse and inventory data structure release

--- server/server.js
+++ server/server.js
@@ -2,12 +2,25 @@ const express = require("express");
 //const cors = require("cors");
 const app = express();
 
+const getWarehouseList = require("./controller/getWarehouseList.js");
+const getItemList = require("./controller/getItemList.js");
+
 // Middleware
 app.use(express.json());
 //app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Home Page");
+});
+
+// api info endpoint for warehouse
+app.get("/warehouse", (req, res) => {
+  res.json(getWarehouseList());
+});
+
+// api info endpoint for inventory item
+app.get("/inventory", (req, res) => {
+  res.json(getItemList());
 });
 
 const PORT = process.env.PORT || 5000;
