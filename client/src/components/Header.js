ba4905f048a0edc6ba975018748a24a14667a126
YJ Seo <helloyjseo@gmail.com>
2020-06-02 22:05:10 -0700

    updated className on Header comp

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,16 +3,16 @@ import { Logos } from "./assets";
 
 function Header() {
   return (
-    <header class="header">
-      <div class="header__container">
+    <header className="header">
+      <div className="header__container">
         <img
-          class="header__logo"
+          className="header__logo"
           src={Logos.instockLogoSvg}
           alt="instock logo"
         />
-        <nav class="header__nav">
-          <span class="header__link header__link--active">Inventory</span>
-          <span class="header__link">Locations</span>
+        <nav className="header__nav">
+          <span className="header__link header__link--active">Inventory</span>
+          <span className="header__link">Locations</span>
         </nav>
       </div>
     </header>
