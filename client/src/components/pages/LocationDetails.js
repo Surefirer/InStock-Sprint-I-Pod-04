4562562c5463aff54eb77ff05766fdc9ef936f29
YJ Seo <helloyjseo@gmail.com>
2020-06-09 22:57:50 -0700

    styling update on Location Details

--- client/src/components/pages/LocationDetails.js
+++ client/src/components/pages/LocationDetails.js
@@ -25,10 +25,10 @@ class LocationDetails extends Component {
   render() {
     return (
       <>
-        <div className="locationdetails-top">
-          {this.state.details.map((d) => {
-            return (
-              <>
+        {this.state.details.map((d) => {
+          return (
+            <>
+              <div className="locationdetails-top">
                 <div
                   className="locationdetails-top-container1"
                   id="the item id"
@@ -70,69 +70,67 @@ class LocationDetails extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="locationdetails-bottom__bodyMobile">
-                  <div
-                    className="locationdetails-bottom__wrapperMobile"
-                    key="key"
-                  >
-                    <div className="locationdetails-bottom__container">
-                      <div className="locationdetails-bottom__container--item">
-                        <h3 className="locationdetails-bottom__title">ITEM</h3>
-                        <h2 className="locationdetails-bottom__product">
-                          Product Name Here
-                        </h2>
-                        <p className="locationdetails-bottom__text">
-                          Here is a very brief description of the product in the
-                          locationdetails-bottom...
-                        </p>
-                      </div>
-                      <div className="locationdetails-bottom__container--order">
-                        <h3 className="locationdetails-bottom__title">
-                          LAST ORDERED
-                        </h3>
-                        <p className="locationdetails-bottom__text">
-                          05/24/2018
-                        </p>
-                      </div>
-                      <div className="locationdetails-bottom__container--location">
-                        <h3 className="locationdetails-bottom__title">
-                          LOCATION
-                        </h3>
-                        <p className="locationdetails-bottom__text">
-                          Toronto, CAN
-                        </p>
-                      </div>
-                      <div className="locationdetails-bottom__container--quantity">
-                        <h3 className="locationdetails-bottom__title">
-                          QUANTITY
-                        </h3>
-                        <p className="locationdetails-bottom__text">12,000</p>
-                      </div>
-                      <div className="locationdetails-bottom__container--status">
-                        <h3 className="locationdetails-bottom__title">
-                          STATUS
-                        </h3>
-                        <p className="locationdetails-bottom__text">In Stock</p>
-                      </div>
+              </div>
+              <div className="locationdetails-bottom__bodyMobile">
+                <div
+                  className="locationdetails-bottom__wrapperMobile"
+                  key="key"
+                >
+                  <div className="locationdetails-bottom__container">
+                    <div className="locationdetails-bottom__container--item">
+                      <h3 className="locationdetails-bottom__title">ITEM</h3>
+                      <h2 className="locationdetails-bottom__product">
+                        Product Name Here
+                      </h2>
+                      <p className="locationdetails-bottom__text">
+                        Here is a very brief description of the product in the
+                        locationdetails-bottom...
+                      </p>
                     </div>
-                    <div className="locationdetails-bottom__iconWrapper">
-                      <img
-                        className="locationdetails-bottom__kebabIcon"
-                        src={Icons.kebabDefault}
-                        alt="locationdetails-bottom kebab icon"
-                      />
-                      <img
-                        className="locationdetails-bottom__kebabIcon--hover"
-                        src={Icons.kebabActive}
-                        alt="locationdetails-bottom kebab active icon"
-                      />
-                      <button className="locationdetails-bottom__remove">
-                        Remove
-                      </button>
+                    <div className="locationdetails-bottom__container--order">
+                      <h3 className="locationdetails-bottom__title">
+                        LAST ORDERED
+                      </h3>
+                      <p className="locationdetails-bottom__text">05/24/2018</p>
+                    </div>
+                    <div className="locationdetails-bottom__container--location">
+                      <h3 className="locationdetails-bottom__title">
+                        LOCATION
+                      </h3>
+                      <p className="locationdetails-bottom__text">
+                        Toronto, CAN
+                      </p>
+                    </div>
+                    <div className="locationdetails-bottom__container--quantity">
+                      <h3 className="locationdetails-bottom__title">
+                        QUANTITY
+                      </h3>
+                      <p className="locationdetails-bottom__text">12,000</p>
+                    </div>
+                    <div className="locationdetails-bottom__container--status">
+                      <h3 className="locationdetails-bottom__title">STATUS</h3>
+                      <p className="locationdetails-bottom__text">In Stock</p>
                     </div>
                   </div>
+                  <div className="locationdetails-bottom__iconWrapper">
+                    <img
+                      className="locationdetails-bottom__kebabIcon"
+                      src={Icons.kebabDefault}
+                      alt="locationdetails-bottom kebab icon"
+                    />
+                    <img
+                      className="locationdetails-bottom__kebabIcon--hover"
+                      src={Icons.kebabActive}
+                      alt="locationdetails-bottom kebab active icon"
+                    />
+                    <button className="locationdetails-bottom__remove">
+                      Remove
+                    </button>
+                  </div>
                 </div>
-                <div className="locationdetails-bottom__bodyTablet">
+              </div>
+              <div className="locationdetails-bottom__bodyTablet">
+                <div className="locationdetails-bottom__wrapper">
                   <div className="locationdetails-bottom__titleWrapper">
                     <h3 className="locationdetails-bottom__title locationdetails-bottom__item">
                       ITEM
@@ -193,10 +191,10 @@ class LocationDetails extends Component {
                     </div>
                   </div>
                 </div>
-              </>
-            );
-          })}
-        </div>
+              </div>
+            </>
+          );
+        })}
       </>
     );
   }
